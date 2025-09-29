68a11592229b4b22851cf89b3e0950976ac9a013
Colin <seay0006@algonquinlive.com>
2025-09-29 11:39:52 -0400

    Completed Assignment

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ const gallery = document.getElementById('emoji-gallery');
 
 // trying to make the emojis show on the page
 emojiList.forEach(emoji => {
+    const emojiContainer = document.createElement('div');
+    emojiContainer.className = 'emoji-container';
+
     const emojiElement = document.createElement('div');
     emojiElement.className = 'emoji';
     emojiElement.textContent = emoji;
-    gallery.appendChild(emojiElement);
+    
+    const codeElement = document.createElement('div');
+    codeElement.className = 'emoji-code';
+    codeElement.textContent = 'U+' + emoji.codePointAt(0).toString(16).toUpperCase();
+    
+    
+    emojiContainer.appendChild(emojiElement);
+    emojiContainer.appendChild(codeElement);
+    gallery.appendChild(emojiContainer);
 }
 );
