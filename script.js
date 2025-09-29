1d15ccfbd85362f68e5d19fc1129df23c00ea8ce
Colin <seay0006@algonquinlive.com>
2025-09-29 10:14:48 -0400

    adding emojis in js and the div for html

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 //1. You may not declare variables with var. Use either const or let.
 
 
+// adding emoji to the gallery
+const emojiList = ['😀', '⌚', '🚀', '🎉', '💗', '🔥', '🌟', '😎', '👍', '🤩','💪','🚆'];
+const gallery = document.getElementById('emoji-gallery');
