d2cdff5d9386edfe6eb81aba62adb6c885ce27c7
Colin <seay0006@algonquinlive.com>
2025-09-29 10:24:13 -0400

    adding emojilist for each emoji to have htem display on site

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 // adding emoji to the gallery
 const emojiList = ['😀', '⌚', '🚀', '🎉', '💗', '🔥', '🌟', '😎', '👍', '🤩','💪','🚆'];
 const gallery = document.getElementById('emoji-gallery');
+
+// trying to make the emojis show on the page
+emojiList.forEach(emoji => {
+    const emojiElement = document.createElement('div');
+    emojiElement.className = 'emoji';
+    emojiElement.textContent = emoji;
+    gallery.appendChild(emojiElement);
+}
+);
